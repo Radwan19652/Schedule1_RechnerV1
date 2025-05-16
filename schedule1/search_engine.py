3df50be4b19d628793b19f338a85e95944d7c1fc
Radwan19652 <rdoan.chaban.51@gmail.com>
2025-05-16 03:10:31 +0200

    Working Program

--- schedule1/search_engine.py
+++ schedule1/search_engine.py
@@ -44,7 +44,13 @@ class SearchEngine:
         abort_callback = abort_callback or (lambda: False)
 
         # Zutatenliste vorbereiten
-        ingredients = allowed_ingredients or list(self.calc.items_data.keys())
+        if allowed_ingredients and len(allowed_ingredients) > 0:
+            # Nur erlaubte Zutaten verwenden, die auch im Calculator existieren
+            ingredients = [ing for ing in allowed_ingredients 
+                        if ing in self.calc.INGREDIENT_PRICES]
+        else:
+            # Wenn keine Zutaten gewählt wurden, alle verfügbaren verwenden
+            ingredients = list(self.calc.INGREDIENT_PRICES.keys())
 
         # Einzel-Ertrag für Heuristik berechnen
         profit_yields: List[Tuple[float,str]] = []
@@ -84,8 +90,17 @@ class SearchEngine:
                 total_sale = self.calc.calculate_sale_price(list(effects), base)
                 total_cost = self.calc.calculate_cost(seq)
                 total_profit = total_sale - total_cost
-                best_seq, best_eff = seq, list(effects)
-                best_profit, best_cost = total_profit, total_cost
+                
+                # Prüfe, ob diese Lösung besser ist als die bisherige
+                is_better = False
+                if optimize_for == "cost":
+                    is_better = (best_seq == [] or total_cost < best_cost)
+                else:  # "profit"
+                    is_better = (best_seq == [] or total_profit > best_profit)
+                    
+                if is_better:
+                    best_seq, best_eff = seq, list(effects)
+                    best_profit, best_cost = total_profit, total_cost
                 break
 
             state = (frozenset(effects), depth)
@@ -102,11 +117,25 @@ class SearchEngine:
                 prof = sale - cost
 
                 steps_left = max_steps - len(new_seq)
-                h = sum(yields_only[:steps_left]) if steps_left > 0 else 0.0
-
-                f_new = -(prof + h)
-                g_new = -prof
-
+                
+                # Bonus für gewünschte Effekte
+                effect_bonus = 0.0
+                if desired_effects:
+                    # Zähle, wie viele der gewünschten Effekte enthalten sind
+                    matched_effects = sum(1 for e in desired_effects if e in new_eff)
+                    effect_bonus = matched_effects * 10.0  # Bonus pro gefundenem Effekt
+                
+                if optimize_for == "cost":
+                    # Bei "cost" optimieren wir auf minimale Kosten
+                    h = 0.0  # Keine Heuristik für Kosten
+                    f_new = cost - effect_bonus  # Je kleiner, desto besser, mit Bonus für Effekte
+                    g_new = cost
+                else:  # "profit" (default)
+                    # Bei "profit" optimieren wir auf maximalen Profit 
+                    h = sum(yields_only[:steps_left]) if steps_left > 0 else 0.0
+                    f_new = -(prof + h + effect_bonus)  # Mit Bonus für Effekte
+                    g_new = -(prof + effect_bonus)
+                
                 heapq.heappush(open_list, (f_new, g_new, new_seq, new_eff))
 
         return best_seq, best_eff, best_cost, best_profit
@@ -153,9 +182,13 @@ class SearchEngine:
                 abort_callback=abort
             )
 
-            if seq and profit > best_profit:
-                best_profit, best_cost = profit, cost
-                best_seq, best_eff = seq, eff
+            if seq:
+                if optimize_for == "cost" and (best_seq == [] or cost < best_cost):
+                    best_profit, best_cost = profit, cost
+                    best_seq, best_eff = seq, eff
+                elif optimize_for == "profit" and (best_seq == [] or profit > best_profit):
+                    best_profit, best_cost = profit, cost
+                    best_seq, best_eff = seq, eff
 
         return best_seq, best_eff, best_cost, best_profit
 
